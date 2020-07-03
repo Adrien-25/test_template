022fa4accde1cf865ea7fc888c9b133d3cdc0695
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-07-03 10:31:55 +0200

    Add - Mise en place du projet

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('template');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+$title = 'Le titre de la page';
+$nom = 'SCHMIDT';
+$template = $twig->load('base.html.twig');
+echo $template->render(['title' => $title, 'nom' => $nom]);
+
+
+
+
+
+
+
